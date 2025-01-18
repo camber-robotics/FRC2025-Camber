39d5183c8fb347d8fe274c6d95ad13fce51f2e41
Nobody <149746398+nobodyhere4@users.noreply.github.com>
2025-01-18 08:40:30 -0600

    DataLog confustion

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -28,7 +29,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    DataLogManager.start();// REMEBER 
+    DataLogManager.start();// REMEBER TO REMOVE WHEN ACTUAL BOT IS HERE 
+    //DriverStation.startDataLog(DataLogManager.getLog());
   }
 
   /**
